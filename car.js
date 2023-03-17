39e7dd3909a8079f056749829f26b397e1c3dc4d
Ankur Rawat <ankurrawat620@gmail.com>
2023-03-17 20:26:48 +0530

    Add Visualizer

--- car.js
+++ car.js
@@ -12,10 +12,15 @@ class Car{
         this.angle = 0;
         this.damage = false;
 
+        this.useBrain = controlType=="AI";
+
         
         this.polygon = this.#createPolygon();
         if(controlType!="DUMMY"){
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount,6, 4]
+            );
         }
         this.controls = new Controls(controlType);
     }
@@ -27,7 +32,20 @@ class Car{
             this.damage = this.#assessDamage(roadBoarders, traffic);
         }
         if(this.sensor)
+        {
             this.sensor.update(roadBoarders, traffic);
+            const offsets = this.sensor.readings.map(
+                s=>s==null?0:1-s.offsets
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
+        }
     }
     
     #assessDamage(roadBoarders, traffic){
