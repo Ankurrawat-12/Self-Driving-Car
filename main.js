b3c558bf083196479c43f08af640be3f30b045b3
Ankur Rawat <ankurrawat620@gmail.com>
2023-03-18 01:13:04 +0530

    Change Size of Network Canvas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
 const networkCanvas=document.getElementById("networkCanvas");
-networkCanvas.width=600;
+networkCanvas.width=500;
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
